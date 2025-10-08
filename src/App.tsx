37bf9b44b5d3777b016f12fc0215943c59f8ba87
K1butsujiMuzan <maks_shishkin_2007@inbox.ru>
2025-10-08 21:38:06 +0200

    feat[header]: add header

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Header from "./Components/Header/Header.tsx";
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/header.data.ts
@@ -0,0 +1,29 @@
+import twitter from "@assets/icons/links/twitter.svg"
+import discord from "@assets/icons/links/discord.svg"
+import youtube from "@assets/icons/links/youtube.svg"
+import telegram from "@assets/icons/links/telegram.svg"
+
+interface ISocials {
+  image: string,
+  alt: string
+}
+
+export const links:string[] = ["Wave NFTs", "Stories", "Contact"]
+export const socials:ISocials[] = [
+  {
+    image: twitter,
+    alt: "twitter"
+  },
+  {
+    image: discord,
+    alt: "discord"
+  },
+  {
+    image: youtube,
+    alt: "youtube"
+  },
+  {
+    image: telegram,
+    alt: "telegram"
+  }
+]
